6eff2079f0c18a4553e880b699c54ac13827e33f
np185137 <nagaraju.peduri@ncr.com>
2020-03-10 16:02:56 +0530

    Added thread termination program using ExitThread

--- windowsInternals/threadExit/threadExit.cpp
+++ windowsInternals/threadExit/threadExit.cpp
@@ -5,11 +5,11 @@
 #include<Windows.h>
 using namespace std;
 
+DWORD exitCode;
 
 DWORD WINAPI exitThread(LPVOID lpParam) {
     cout << "Hey there, you have reached here to terminate the thread." << endl;
-    exitThread(lpParam);
-    return 0;
+    ExitThread(exitCode);       //terminate the thread using exitCode.
 }
 
 
@@ -17,13 +17,29 @@ int main()
 {
     unsigned int counter = 0;
     DWORD threadId;
+    //DWORD exitCode = 0;
 
     HANDLE myHandle = CreateThread(
-        NULL,
-        0,
-        exitThread,
-        &counter,
+        NULL,       //allow returned handle inherit by child process.
+        0,          //stack size
+        exitThread, //callback funtion(start function).
+        &counter,   //lpparameter, which is used as parameter in callback function.
         0,
         &threadId
     );
+
+    if (myHandle == 0) {
+        cout << "Thread creation failed due to error: " << GetLastError() << endl;
+    }
+    else {
+       // WaitForSingleObject(myHandle, INFINITE);
+        if (!GetExitCodeThread(myHandle, &exitCode)) {    //First get the exit code of the thread. Using this, terminate the thread in its callback function. 
+            cout << "Cannot get the thread exit code due to the error: " << GetLastError() << endl;
+        }
+        else{
+            WaitForSingleObject(myHandle, 100);
+            cout << exitCode;
+        }      
+    }
+    return 0;
 }
