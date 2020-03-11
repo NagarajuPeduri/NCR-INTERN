d7e7412d938b3e4ae56a57a6205b720d7d031543
np185137 <nagaraju.peduri@ncr.com>
2020-03-11 12:46:53 +0530

    Did some changes

--- windowsInternals/inheritHandleByChildProcess/inheritHandleByChildProcess.cpp
+++ windowsInternals/inheritHandleByChildProcess/inheritHandleByChildProcess.cpp
@@ -8,8 +8,8 @@ using namespace std;
 int main()
 {
 	BOOL processStatus;
-	PROCESS_INFORMATION processInfo;
-	STARTUPINFO startInfo;
+	PROCESS_INFORMATION processInfo, processInfo1;
+	STARTUPINFO startInfo, startInfo1;
 	SECURITY_ATTRIBUTES secAtt;
 
 	secAtt.bInheritHandle = TRUE;		//allow inheritance
@@ -19,6 +19,10 @@ int main()
 	ZeroMemory(&startInfo, sizeof(startInfo));
 	startInfo.cb = sizeof(startInfo);
 	ZeroMemory(&processInfo, sizeof(processInfo));
+
+	ZeroMemory(&startInfo1, sizeof(startInfo1));
+	startInfo1.cb = sizeof(startInfo1);
+	ZeroMemory(&processInfo1, sizeof(processInfo1));
 	
 	//process creation
 	processStatus = CreateProcess(
@@ -39,11 +43,37 @@ int main()
 	}
 	else {
 		WaitForSingleObject(processInfo.hProcess, INFINITE);
-		cout << "Child Process Details:\nProcess ID: " << GetProcessId(processInfo.hProcess) << "  |  Thread ID: " << GetThreadId(processInfo.hThread) << endl;
+		cout << "Notepad process Details:\nProcess ID: " << GetProcessId(processInfo.hProcess) << "  |  Thread ID: " << GetThreadId(processInfo.hThread) << endl;
+	}
+
+	WCHAR handle[] = { (WCHAR)processInfo.hProcess };
+
+	//creating child process
+	processStatus = CreateProcess(
+		L"C:\\Windows\\System32\\calc.exe",
+		handle,			//inherits the parent handle
+		&secAtt,
+		NULL,
+		FALSE,		//doesn't allow inheritance
+		0,
+		NULL,
+		NULL,
+		&startInfo1,
+		&processInfo1
+	);
+
+	if (processStatus == 0) {
+		cout << "Process creation failed due to: " << GetLastError() << endl;
+	}
+	else {
+		WaitForSingleObject(processInfo1.hProcess, INFINITE);
+		cout << "\nalc process Details:\nProcess ID: " << GetProcessId(processInfo1.hProcess) << "  |  Thread ID: " << GetThreadId(processInfo1.hThread) << endl;
 	}
 
 	CloseHandle(processInfo.hThread);
 	CloseHandle(processInfo.hProcess);
+	CloseHandle(processInfo1.hThread);
+	CloseHandle(processInfo1.hProcess);
 
 	return 0;
 }
